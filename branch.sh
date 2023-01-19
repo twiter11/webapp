26fd8a25aca2187115e2279e01f6f3dc2b7da240
charles okpataku <charlesokpataku@gmail.com>
2023-01-19 03:29:41 +0000

    script for creating branches

---
+++ branch.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+#this script will create a git branch in linux server
+echo "git branch dev & stage about to be created.."
+git branch dev
+git branch stage
+echo "git branches created successfully"
+git branch
+date
