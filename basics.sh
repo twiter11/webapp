9dc6e5535c07d2d87ba42385ac9d10117613092d
CharlesOkpataku <122658634+CharlesOkpataku@users.noreply.github.com>
2023-01-30 13:42:31 +0100

    Create basics.sh

---
+++ basics.sh
@@ -0,0 +1,5 @@
+#this script will instal: wget, vim, nano, tree, unzip & git to linux and linux redhrts server
+sudo yum install wget nano vim tree unzip git-all -y
+echo "Applications installed successfully"
+echo "THANK YOU FOR USING THIS SCRIPT"
+date
